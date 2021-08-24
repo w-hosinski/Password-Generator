32e9cbdf84c23a1d7cb14e555b9bc40d9e27f44b
w-hosinski <woj.hosinski@gmail.com>
2021-08-24 18:44:03 +0200

    Typescript fixes
    Typescript fixes und minChars 6 zu 8

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-submit.addEventListener("click",generate);
-validate.addEventListener("click",validation);
-let checkedCount = 0;
-let output = "";
+submit.addEventListener("click",generate)
+validate.addEventListener("click",validation)
+let checkedCount = 0
+let output = ""
 function generate() {
-    document.getElementById("output").value = "";
-    output = "";
-    let randomSet = "";
-    checkedCount = document.querySelectorAll('input[class="inputCheckbox"]:checked').length;
-    let length = parseInt(document.getElementById("length").value);
-    if(document.getElementById("lowerCase").checked == true) randomSet += "A";
-    if(document.getElementById("upperCase").checked == true) randomSet += "B";
-    if(document.getElementById("numbers").checked == true) randomSet += "C";
-    if(document.getElementById("specChars").checked == true) randomSet += "D";
+    document.getElementById("output").value = ""
+    output = ""
+    let randomSet = ""
+    checkedCount = document.querySelectorAll('input[class="inputCheckbox"]:checked').length
+    let length = parseInt(document.getElementById("length").value)
+    if(document.getElementById("lowerCase").checked == true) randomSet += "A"
+    if(document.getElementById("upperCase").checked == true) randomSet += "B"
+    if(document.getElementById("numbers").checked == true) randomSet += "C"
+    if(document.getElementById("specChars").checked == true) randomSet += "D"
     for(i=0; i<length; i++){
     switch (randomSet.charAt(Math.floor(Math.random() * checkedCount))) {
-        case "A": genLowerCase(); 
+        case "A": genLowerCase() 
         break
-        case "B": genUpperCase();
+        case "B": genUpperCase()
         break
-        case "C": genNumber();
+        case "C": genNumber()
         break
-        case "D": genSpecial();
+        case "D": genSpecial()
         break
     }   
     }
-    document.getElementById("output").value = output; 
+    document.getElementById("output").value = output 
 }
 function genLowerCase(){
 const CHARSET = "abcdefghijklmnopqrstuvwxyz"
-    output += CHARSET.charAt(Math.floor(Math.random() * 26));
+    output += CHARSET.charAt(Math.floor(Math.random() * 26))
 }
 function genUpperCase(){
 const CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    output += CHARSET.charAt(Math.floor(Math.random() * 26));
+    output += CHARSET.charAt(Math.floor(Math.random() * 26))
 }
 function genNumber(){
 const CHARSET = "0123456789"
-    output += CHARSET.charAt(Math.floor(Math.random() * 10));
+    output += CHARSET.charAt(Math.floor(Math.random() * 10))
 }
 function genSpecial(){
 const CHARSET = "!\"#$%&'()*+,-./:;<=>?@[]\\^_`{}|~";
-    output += CHARSET.charAt(Math.floor(Math.random() * 32));
+    output += CHARSET.charAt(Math.floor(Math.random() * 32))
 }
 function validation(){
-    document.getElementById("minLength").checked = "";
-    document.getElementById("maxLength").checked = "";
-    document.getElementById("minLowerCase").checked = "";
-    document.getElementById("minUpperCase").checked = "";
-    document.getElementById("minNumber").checked = "";
-    document.getElementById("minSpecChar").checked = "";
-    const CHARSET = "!\"#$%&'()*+,-./:;<=>?@[]\\^_`{}|~";
-    const INPUT = document.getElementById("input").value;
-    let minLength = /^.{6,}$/;
-    let maxLength = /^.{1,20}$/;
-    let minLower = /^(?=(?:.*?[a-z]){2})/;
-    let minUpper = /^(?=(?:.*?[A-Z]){2})/;
-    let minNumber = /^(?=(?:.*?\d){2})/;
-    //let minSpecial = 1;
-    if(minLength.test(INPUT)) document.getElementById("minLength").checked = "checked";
-    if(maxLength.test(INPUT)) document.getElementById("maxLength").checked = "checked";
-    if(minLower.test(INPUT)) document.getElementById("minLowerCase").checked = "checked";
-    if(minUpper.test(INPUT)) document.getElementById("minUpperCase").checked = "checked";
-    if(minNumber.test(INPUT)) document.getElementById("minNumber").checked = "checked";
-    //if(minSpecial.test(INPUT)) document.getElementById("minSpecChar").checked = "checked";
+    document.getElementById("minLength").checked = ""
+    document.getElementById("maxLength").checked = ""
+    document.getElementById("minLowerCase").checked = ""
+    document.getElementById("minUpperCase").checked = ""
+    document.getElementById("minNumber").checked = ""
+    document.getElementById("minSpecChar").checked = ""
+    const CHARSET = "!\"#$%&'()*+,-./:;<=>?@[]\\^_`{}|~"
+    const INPUT = document.getElementById("input").value
+    let minLength = /^.{8,}$/
+    let maxLength = /^.{1,20}$/
+    let minLower = /^(?=(?:.*?[a-z]){2})/
+    let minUpper = /^(?=(?:.*?[A-Z]){2})/
+    let minNumber = /^(?=(?:.*?\d){2})/
+    //let minSpecial = 1
+    if(minLength.test(INPUT)) document.getElementById("minLength").checked = "checked"
+    if(maxLength.test(INPUT)) document.getElementById("maxLength").checked = "checked"
+    if(minLower.test(INPUT)) document.getElementById("minLowerCase").checked = "checked"
+    if(minUpper.test(INPUT)) document.getElementById("minUpperCase").checked = "checked"
+    if(minNumber.test(INPUT)) document.getElementById("minNumber").checked = "checked"
+    //if(minSpecial.test(INPUT)) document.getElementById("minSpecChar").checked = "checked"
 }
